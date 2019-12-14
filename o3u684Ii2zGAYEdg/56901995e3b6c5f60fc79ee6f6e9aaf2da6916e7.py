709fa25367aaf346945775f9d5f75148417084ea
vmlankub <45290401+vmlankub@users.noreply.github.com>
2019-12-14 15:10:53 +0800

    @MinecraftFuns: Original file: C:/Users/Administrator/Desktop/WorkSpace/upload/settings.py

---
+++ o3u684Ii2zGAYEdg/56901995e3b6c5f60fc79ee6f6e9aaf2da6916e7.py
@@ -0,0 +1,14 @@
+# settings
+u_token = "a1be4b7e6d9ffe352765b3dcda4c02f20b24642e"
+base_url = "https://api.github.com/"
+user_name = "vmlankub"
+user_mail = "45290401+vmlankub@users.noreply.github.com"
+account_name = "urlib"
+repo_name = "hJRvfDVc"
+simple_repo_name = "arctic"
+proxy_repo_name = "sorry"
+image_repo_name = "tim"
+gen_method = "sha1"
+
+proxies = {"http": "http://127.0.0.1:1080",
+           "https": "http://127.0.0.1:1080"}
